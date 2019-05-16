04820410e8f0953d854143faa7c333d18d8d7eb5
Andreas Kuoppa <andreas.kuoppa@gmail.com>
2019-05-16 09:20:13 +0200

    Created front page showing all images. Prepared for upload page.

--- upload/views.py
+++ upload/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView
+from .models import Photo
+
+
+class HomePageView(ListView):
+    model = Photo
+    template_name = 'home.html'

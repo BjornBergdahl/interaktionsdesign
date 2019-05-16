c83e1e6394eb5ce379d3bf70279565f4888eb2bb
Andreas Kuoppa <andreas.kuoppa@gmail.com>
2019-05-16 08:58:01 +0200

    Fixed database model

--- upload/models.py
+++ upload/models.py
@@ -3,7 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Photo(models.Model):
-    preview_blob = models.BinaryField()
-    full_blob = models.BinaryField()
+    title = models.TextField()
+    cover = models.ImageField(upload_to = 'images/')
+    preview_blob = models.BinaryField() # unneccessary?
+    full_blob = models.BinaryField()    # unneccessary? 
     uploader_id = models.IntegerField()
     upload_date = models.DateField()
+
+    def __str__(self):
+        return self.title

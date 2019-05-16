bc631be982709c8c66cf46144282f5bd3ffe53cd
Andreas Kuoppa <andreas.kuoppa@gmail.com>
2019-05-16 09:50:28 +0200

    Functionality for upload and viewing working

--- upload/models.py
+++ upload/models.py
@@ -5,10 +5,8 @@ from django.db import models
 class Photo(models.Model):
     title = models.TextField()
     cover = models.ImageField(upload_to = 'images/')
-    preview_blob = models.BinaryField() # unneccessary?
-    full_blob = models.BinaryField()    # unneccessary? 
-    uploader_id = models.IntegerField()
-    upload_date = models.DateField()
+    uploader_id = models.IntegerField(blank=True, null=True)
+    upload_date = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return self.title
